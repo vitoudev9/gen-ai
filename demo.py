c6900f258027e6adad03b275cd9bd1e639f2b641
vitoudev9 <vitoudev9@gmail.com>
2025-01-23 08:18:21 +0700

    commit new bot

--- demo.py
+++ demo.py
@@ -1,30 +1,20 @@
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
-from langchain_aws import ChatBedrock
 import boto3
 import os
 import streamlit as st
 
-# install langchain, langchain_aws, boto3, streamlit
-# configure AWS_Profile and check credential in console
-
-
 os.environ['AWS_PROFILE'] = 'auralis'
 
 bedrock_client = boto3.client(
-    service_name="bedrock-runtime",
+    service_name="bedrock-agent-runtime",
     region_name="us-west-2"
 )
 
 modelID = "anthropic.claude-3-haiku-20240307-v1:0"
-
-
-llm = ChatBedrock(
-    model_id=modelID,
-    client=bedrock_client,
-    ##model_kwargs={"max_tokens_to_sample": 2000,"temperature":0.9}
-)
+agentAliasID = "GM7PZJG8MI"
+agentID = "4BNFDCQ7NJ"
+sessionID = "2dd318f3-54d9-48c7-8b72-022759e6a79b"
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 
@@ -32,30 +22,12 @@ def my_chatbot():
 
     st.title("Auralis Chatbot")
 
-    prompt = PromptTemplate(
-        input_variables=["chat_history", "text_input"],
-        template = """
-        The following is a friendly conversation between a user and an AI assistant. The assistant remembers past interactions and provides helpful responses.
-
-        Chat History:
-        {chat_history}
-
-        User: {text_input}
-        AI:
-        """
-        #template="You are a chatbot. You are in {language}.\n\n{freeform_text}"
-    )
-
-    bedrock_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
-
     # Session state to store conversation history
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-    #if prompt := st.chat_input():
     
     for message in st.session_state.chat_history:
         with st.chat_message(message["role"]):
@@ -68,10 +40,36 @@ def my_chatbot():
             st.markdown(input_text)
         st.session_state.chat_history.append({"role":"user", "text":input_text })
 
-        chat_response = bedrock_chain.run({"text_input": input_text})
+        print("sessionId= " + sessionID)
+        response = bedrock_client.invoke_agent(
+            enableTrace=True,
+            sessionId=sessionID,
+            agentAliasId=agentAliasID,
+            agentId=agentID,
+            inputText=input_text
+        )
+
+        chat_response = process_response(response)
+
         with st.chat_message("assistant"):
             st.markdown(chat_response)
         st.session_state.chat_history.append({"role":"assistant", "text":chat_response })
+        print(chat_response)
+
+
+def process_response(response):
+    completion = ''
+    for event in response.get('completion'):
+        if 'chunk' in event:
+            chunk = event["chunk"]
+            #print('\n- chunk', chunk)
+            completion = completion + chunk["bytes"].decode()
+        
+        elif 'trace' in event:
+            trace = event["trace"]
+            #print('\n- trace', trace)
+
+    return completion
 
 my_chatbot()
 
